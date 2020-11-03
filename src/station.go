12ab484f1efeac0a578d54c5d3f2d0af7043dfa9
Fernando Roldan <Doskoy93@gmail.com>
2020-11-03 16:36:07 +0100

    Creating the class for stations

---
+++ src/station.go
@@ -0,0 +1,10 @@
+package station
+
+type Station struct {
+	id              string
+	adr             string
+	city            string
+	total_slots     int
+	available_bikes int
+	free_slots      int
+}
