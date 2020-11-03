0563db3867dab5e6b3dd37febca7f3897fcdae70
Fernando Roldan <Doskoy93@gmail.com>
2020-11-03 16:38:30 +0100

    Adding functions for checking station occupancy #9

--- src/station.go
+++ src/station.go
@@ -8,3 +8,23 @@ type Station struct {
 	available_bikes int
 	free_slots      int
 }
+
+func (s Station) String() string{
+  return fmt.Sprintf("\nId: %v\n Adress: %v\n total_slots: %v\n Bikes: %v\n Empty slots: %v\n", s.id, s.adr, s.total_slots, s.available_bikes, s.free_slots)
+}
+
+func (s Station) bool check_free_slots(){
+  result := false
+  if s.free_slots > 0 {
+    result = true
+  }
+  return result
+}
+
+func (s Station) bool check_bikes(){
+  result := false
+  if s.available_bikes > 0 {
+    result = true
+  }
+  return result
+}
