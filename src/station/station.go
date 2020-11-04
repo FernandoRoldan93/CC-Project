80f8461dd989e70a37ebebd3c4a4bd58858896a1
Fernando Roldan <Doskoy93@gmail.com>
2020-11-04 21:34:57 +0100

    Fixing golang syntax errors and folder structure

---
+++ src/station/station.go
@@ -0,0 +1,56 @@
+package station
+
+import "fmt"
+
+type Station struct {
+	id             string
+	adr            string
+	city           string
+	totalSlots     int
+	availableBikes int
+	freeSlots      int
+}
+
+func (s Station) String() string {
+	return fmt.Sprintf("\nId: %v\n Adress: %v\n total_slots: %v\n Bikes: %v\n Empty slots: %v\n", s.id, s.adr, s.totalSlots, s.availableBikes, s.freeSlots)
+}
+
+func checkFreeSlots(s Station) bool {
+	result := false
+	if s.freeSlots > 0 {
+		result = true
+	}
+	return result
+}
+
+func checkBikes(s Station) bool {
+	result := false
+	if s.availableBikes > 0 {
+		result = true
+	}
+	return result
+}
+
+func rentBike(s Station) string {
+	result := ""
+	if checkBikes(s) == true {
+		s.availableBikes--
+		s.freeSlots++
+		result = "1: ok "
+	} else {
+		result = "0: ERROR, There are not any bike to rent"
+	}
+	return result
+}
+
+func storeBike(s Station) string {
+	result := ""
+	if checkFreeSlots(s) == true {
+		s.availableBikes++
+		s.freeSlots--
+		result = "1: ok "
+	} else {
+		result = "0: ERROR, There are not any free slot"
+	}
+	return result
+}
